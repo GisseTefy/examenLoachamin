78ce5faa34433acde8e9bf226f9433798685ce75
GisseTefy <gisselle.estefania1997@gmail.com>
2023-12-20 20:54:34 -0500

    Domain corregidos

--- examen/src/main/java/ec/edu/espe/examenloachamin/examen/domain/Alumno.java
+++ examen/src/main/java/ec/edu/espe/examenloachamin/examen/domain/Alumno.java
@@ -1,5 +1,36 @@
 package ec.edu.espe.examenloachamin.examen.domain;
 
-public class Alumno {
+import java.sql.Timestamp;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Date;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ALUMNO")
+
+public class Alumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "COD_ALUMNO",nullable = false)
+    private Integer codAlumno;
+    @Column (name = "COD_COLEGIO",nullable = false)
+    private Integer codColegio;
+
+    @Column (name = "CEDULA",nullable = false)
+    private String cedula;
+    @Column (name = "NOMBRE",nullable = false)
+    private String nombre;
+    @Column (name = "FECHA_NACIMIENTO",nullable = false)
+    private Date fechaNacimiento;
+    @Column (name = "FECHA_CREACION",nullable = false)
+    private Timestamp fechaCreacion;
+
+    @ManyToOne()
+    @JoinColumn(name = "COD_COLEGIO", nullable = false, updatable = false, insertable = false)
+    private Colegio colegio;
 }
--- examen/src/main/java/ec/edu/espe/examenloachamin/examen/domain/Asignatura.java
+++ examen/src/main/java/ec/edu/espe/examenloachamin/examen/domain/Asignatura.java
@@ -1,5 +1,18 @@
 package ec.edu.espe.examenloachamin.examen.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name = "ASIGNATURA")
 public class Asignatura {
+    @EmbeddedId
+    @Column (name = "COD_ASIGNATURA",nullable = false)
+    private String codAsignatura;
+
+    @Column (name = "NOMBRE",nullable = false)
+    private String nombre;
 
 }
